481404ce653dc8376203212966af7d6256fd6fdf
Gargantua <martinzb949@gmail.com>
2020-12-29 17:23:42 -0800

    modifed my server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // Import express-session
 const session = require("express-session");
 
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -19,8 +22,6 @@ const sess = {
 };
 app.use(session(sess));
 
-const sequelize = require("./config/connection");
-
 const hbs = exphbs.create({});
 
 app.engine("handlebars", hbs.engine);
@@ -30,7 +31,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require("./controllers/"));
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log("Now listening"));
