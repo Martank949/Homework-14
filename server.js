7c2035e8e09ebcc168e7a68bd5f079ec3f6dea54
Gargantua <martinzb949@gmail.com>
2021-01-11 15:48:27 -0800

    removed duplicate routes

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 const session = require("express-session");
-const routes = require("./controllers");
 const helpers = require("./utils/helpers");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 // Import express-session
