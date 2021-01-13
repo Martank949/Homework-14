86e98d151793d170f0b176057c950623c359770c
Gargantua <martinzb949@gmail.com>
2021-01-12 18:22:48 -0800

    ok

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
+const seeder = require("../seeds/seed.js");
 
 const apiRoutes = require("./api");
 const homeRoutes = require("./homeRoutes");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.get("/api/seeder/:password", (req, res) => {
+    if (req.params.password === process.env.secret) {
+        seeder();
+    }
+});
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -5,21 +5,25 @@ const userData = require("./userData.json");
 const blogData = require("./blogData.json");
 
 const seedDatabase = async() => {
-    await sequelize.sync({ force: true });
+    try {
+        await sequelize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    for (const blog of blogData) {
-        await Blog.create({
-            ...blog,
-            blog_id: users[Math.floor(Math.random() * users.length)].id,
+        const users = await User.bulkCreate(userData, {
+            individualHooks: true,
+            returning: true,
         });
-    }
 
+        for (const blog of blogData) {
+            await Blog.create({
+                ...blog,
+                blog_id: users[Math.floor(Math.random() * users.length)].id,
+            });
+        }
+        console.log("seeding successful!");
+    } catch (err) {
+        console.log(err);
+    }
     process.exit(0);
 };
 
-seedDatabase();
+module.exports = seedDatabase;
